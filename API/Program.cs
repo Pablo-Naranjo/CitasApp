d7c9f2fa80eb70efc82b5520cfc18f6403eabb25
Pablo-Naranjo <pabnar1205@gmail.com>
2023-09-11 07:05:48 -0600

    Seccion03 - Commit inicial SPA

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,10 +11,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .WithOrigins("https://localhost:4200"));
 app.UseAuthorization();
 
 app.MapControllers();
